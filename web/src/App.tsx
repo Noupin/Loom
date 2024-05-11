a7dbf6ef61a33cdcda7063370cbbfe9b7b487111
Noah-OSU <naperkins7@gmail.com>
2024-05-10 20:53:42 -0400

    updated log group in aws

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <main className="flex flex-row w-full h-full bg-red-700 text-gray-200">
       <div className="flex-1"></div>
       <div className="flex-[7] bg-white bg-opacity-5 rounded-lg m-5 px-5 py-4">
-        Hello
+        Hello there!
       </div>
       <div className="flex-1"></div>
     </main>
