657ffeafd2ef0610ced7cbcd244f20fb0962ef22
Noah-OSU <naperkins7@gmail.com>
2024-05-13 19:00:24 -0400

    added logo to every page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,11 +4,28 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
 import { RecoilRoot } from "recoil";
+import logoForLight from "./assets/logoForLight.svg";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RecoilRoot>
-      <RouterProvider router={router} />
+      <>
+        <React.StrictMode>
+          <RecoilRoot>
+            <>
+              <img
+                src={logoForLight}
+                alt="Logo"
+                height={50}
+                width={50}
+                className="absolute top-[25px] left-[25px]"
+              />
+              <RouterProvider router={router} />
+            </>
+          </RecoilRoot>
+        </React.StrictMode>
+        <RouterProvider router={router} />
+      </>
     </RecoilRoot>
   </React.StrictMode>
 );
