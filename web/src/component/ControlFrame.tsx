7ae3d23dfe7e8ac67451f6f741bf6a1a2af86dc8
Noah-OSU <naperkins7@gmail.com>
2024-05-28 14:04:29 -0400

    added control frame to remove reused styles

---
+++ web/src/component/ControlFrame.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { cn } from "../helper/cn";
+
+interface ControlFrameProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  children: JSX.Element;
+}
+
+const ControlFrame: React.FC<ControlFrameProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  const classNames = cn(
+    className,
+    "flex bg-black bg-opacity-25 rounded-full items-center justify-center border-2 border-white border-opacity-50 hover:bg-opacity-50"
+  );
+  return (
+    <div className={classNames} {...props}>
+      {children}
+    </div>
+  );
+};
+
+export default ControlFrame;
