d9d843c6ae5ce2f59e90e342102569e638a49969
Noah-OSU <naperkins7@gmail.com>
2024-06-05 22:08:18 -0400

    animating search

--- web/src/component/LandingNavigation.tsx
+++ web/src/component/LandingNavigation.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 import { Search } from "lucide-react";
+import { useAnimationPipeline } from "../helper/animation";
+import { TAnimateStatus } from "../types/TAnimation";
 
 interface LandingNavigationProps {
   expandSearch: boolean;
@@ -9,41 +11,123 @@ interface LandingNavigationProps {
 const LandingNavigation: React.FC<LandingNavigationProps> = ({
   expandSearch,
   setExpandSearch,
-}) => (
-  <div className="h-[50px] flex mt-[25px] mx-[25px] items-center relative z-[1]">
-    <div className="flex-1" />
-    <div className="flex-[2] flex text-center text-2xl">
-      <div className="flex-1">Textiles</div>
-      <div className="flex-1">Genres</div>
-      <div className="flex-1">Platform</div>
-    </div>
-    {expandSearch ? (
+}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const isPipelineRunning = useRef(false);
+  const [cancelState, setCancelState] = useState(false);
+  const runAnimationPipeline = useAnimationPipeline(
+    cancelState,
+    setCancelState,
+    isPipelineRunning
+  );
+
+  const [animationState, setAnimationState] = useState<{
+    [key: string]: TAnimateStatus;
+  }>({
+    flipSearchIcon: TAnimateStatus.START,
+    expandSearch: TAnimateStatus.START,
+    fadeSearchText: TAnimateStatus.START,
+  });
+  const AnimationTimings = {
+    flipSearchIcon: 200,
+    expandSearch: 300,
+    fadeSearchText: 100,
+  };
+  const animations = [
+    {
+      animationKeys: ["flipSearchIcon"],
+      durations: [AnimationTimings.flipSearchIcon],
+    },
+    {
+      animationKeys: ["expandSearch"],
+      durations: [AnimationTimings.expandSearch],
+    },
+    {
+      animationKeys: ["fadeSearchText"],
+      durations: [AnimationTimings.fadeSearchText],
+    },
+  ];
+
+  useEffect(() => {
+    if (!expandSearch) {
+      setCancelState(true);
+      setAnimationState({
+        flipSearchIcon: TAnimateStatus.START,
+        expandSearch: TAnimateStatus.START,
+        fadeSearchText: TAnimateStatus.START,
+      });
+      return;
+    }
+    if (isPipelineRunning.current) return;
+    runAnimationPipeline(setAnimationState, animations);
+  }, [expandSearch]);
+
+  const expandSearchCondition =
+    animationState.expandSearch === TAnimateStatus.DONE && expandSearch;
+  const flipSearchIconCondition =
+    animationState.flipSearchIcon === TAnimateStatus.DONE && expandSearch;
+  const fadeSearchTextCondition =
+    animationState.fadeSearchText === TAnimateStatus.DONE && expandSearch;
+
+  return (
+    <div className="h-[50px] flex mt-[25px] mx-[25px] items-center relative z-[1]">
+      <div className="flex-1" />
+      <div className="flex-[2] flex text-center text-2xl">
+        <div className="flex-1">Textiles</div>
+        <div className="flex-1">Genres</div>
+        <div className="flex-1">Platform</div>
+      </div>
       <div
-        className="flex flex-[2] ml-5 justify-end"
-        onMouseLeave={() => setExpandSearch(false)}
-        autoFocus={true}
+        className="flex ml-5 justify-end transition-[flex-grow]"
+        style={{
+          flexGrow: expandSearchCondition ? 2 : 1,
+          transitionDuration: `${AnimationTimings.expandSearch}ms`,
+        }}
       >
         <div
-          className="flex px-3 py-2 bg-black bg-opacity-25 rounded-full transition-[width]"
+          className="flex justify-start px-3 py-2 box-content rounded-full transition-[width] text-clip"
           style={{
-            width: expandSearch ? "75%" : "25px",
-            transitionDuration: "500ms",
+            width: expandSearchCondition ? "75%" : "25px",
+            transitionDuration: `${AnimationTimings.expandSearch}ms`,
+            backgroundColor: expandSearchCondition
+              ? "rgba(0, 0, 0, 0.15)"
+              : "transparent",
+          }}
+          onMouseLeave={() => {
+            if (searchTerm.length > 0) return;
+            setExpandSearch(false);
+          }}
+          onMouseEnter={() => {
+            setExpandSearch(true);
           }}
         >
-          <Search transform="scale(-1, 1)" />
+          <Search
+            className="transition-transform"
+            style={{
+              transitionDuration: `${AnimationTimings.flipSearchIcon}ms`,
+            }}
+            transform={flipSearchIconCondition ? "scale(-1, 1)" : "scale(1, 1)"}
+          />
           <input
             type="text"
-            className="flex-1 ml-1 placeholder-black placeholder-opacity-50 bg-transparent border-none outline-none"
+            className="transition-[flex-grow] ml-1 placeholder-black placeholder-opacity-50 bg-transparent border-none outline-none"
             placeholder="Search..."
+            autoFocus={true}
+            style={{
+              display: fadeSearchTextCondition ? "flex" : "none",
+              flexGrow: fadeSearchTextCondition ? 1 : 0,
+              transitionDuration: `${AnimationTimings.fadeSearchText}ms`,
+            }}
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+              if (e.target.value.length === 0) setExpandSearch(false);
+            }}
           />
         </div>
       </div>
-    ) : (
-      <div className="flex flex-1 justify-end">
-        <Search onMouseEnter={() => setExpandSearch(true)} />
-      </div>
-    )}
-  </div>
-);
+    </div>
+  );
+};
 
 export default LandingNavigation;
