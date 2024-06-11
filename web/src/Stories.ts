b87a0e087200e2429de963fd73d3f0eacfb48ed5
Noah-OSU <naperkins7@gmail.com>
2024-06-10 21:42:19 -0400

    updated story constant -deploy

--- web/src/Stories.ts
+++ web/src/Stories.ts
@@ -19,18 +19,17 @@ export const STORIES: IStory[] = [
       "In a world where monsters roam, a terminally ill woman and her monster-hunting lover grapple with the fleeting nature of time and love, only to face a transformative and tragic twist.",
     image:
       "https://s3.amazonaws.com/static.rogerebert.com/uploads/review/primary_image/reviews/abraham-lincoln-vampire-hunter-2012/EB20120620REVIEWS120629989AR.jpg",
-    datePublished: "06June2024",
+    datePublished: "20June2024",
     link: "/my-x-the-vampire-slayer",
   },
   {
     title: "Android Tragedy",
-    contributors: ["Jane Doe", "Mary Sue"],
+    contributors: ["Noah Perkins"],
     timeToRead: 13,
-    genres: ["Science Fiction", "Drama", "Romance", "16+"],
-    description:
-      "In the year 2042, a sentient android named Artemis escapes its creators, embarking on a journey of self-discovery and facing the harsh realities of a world wary of artificial intelligence.",
+    genres: ["Science Fiction", "Romance", "16+"],
+    description: "Ha! You wish there was a summary ready.",
     image: "https://img.freepik.com/premium-photo/woman-rain_869640-17071.jpg",
-    datePublished: "07Apr2024",
+    datePublished: "23Aug2024",
     link: "/android-tragedy",
   },
 ];
