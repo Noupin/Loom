422843f9314070850e95a08717691a5912a216cd
Noah-OSU <naperkins7@gmail.com>
2024-06-17 21:48:21 -0400

    adding components

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -32,5 +35,17 @@ module.exports = {
       lateef: ["'Lateef'"],
     },
   },
-  plugins: ["tailwindcss-animate"],
+  plugins: ["tailwindcss-animate", addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
