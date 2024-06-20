d7662bb119cba623d857f95d9bc1de6cb6ec183d
Noah-OSU <naperkins7@gmail.com>
2024-06-20 18:40:56 -0400

    removed strict for production experience

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
 );
