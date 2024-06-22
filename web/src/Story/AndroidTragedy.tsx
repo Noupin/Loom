11e1d025ad44b58b915249801d373485a5101a68
Noah-OSU <naperkins7@gmail.com>
2024-06-21 21:54:52 -0400

    updated to reflect not soon release

--- web/src/Story/AndroidTragedy.tsx
+++ web/src/Story/AndroidTragedy.tsx
@@ -3,6 +3,15 @@ import { logoState } from "../State";
 import { useEffect, useState } from "react";
 import { TLogo } from "../types/TLogo";
 import { useAnimateColor } from "../hook/animateColor";
+import { TypewriterEffect } from "../component/TypeWriterEffect";
+
+const words = [
+  { text: "Coming" },
+  { text: "not" },
+  { text: "that" },
+  { text: "soon" },
+  { text: ":)", className: "text-blue-300 dark:text-blue-300" },
+];
 
 export default function AndroidTragedy() {
   const setLogoType = useSetRecoilState(logoState);
@@ -16,15 +25,18 @@ export default function AndroidTragedy() {
 
   return (
     <main
-      className="flex flex-row w-full h-full transition-colors text-white"
+      className="flex flex-col w-full h-full transition-colors text-white"
       style={{
         backgroundColor: bgColor,
       }}
     >
       <div className="flex-1"></div>
       <div className="flex-[7] bg-white bg-opacity-5 rounded-lg m-5 px-5 py-4">
-        <div className="flex justify-center items-center w-full h-full">
-          <div>Androids coming not as soon...</div>
+        <div className="flex flex-col justify-center items-center w-full h-full">
+          <TypewriterEffect words={words} cursorClassName="bg-blue-300" />
+          <p className="mt-5">
+            (I mean I wanna write and make a game first AND write this story so)
+          </p>
         </div>
       </div>
       <div className="flex-1"></div>
